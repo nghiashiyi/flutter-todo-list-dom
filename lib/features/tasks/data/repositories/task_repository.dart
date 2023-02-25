f1f3b97ecbfe7f6dbf05ce229496a8ec62823715
nghia <hi@nghia.tech>
2023-02-25 14:39:01 +0700

    feat: create task repo impl

---
+++ lib/features/tasks/data/repositories/task_repository.dart
@@ -0,0 +1,63 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_todo_list_dom/core/errors/failure.dart';
+import 'package:flutter_todo_list_dom/features/tasks/data/data_sources/task_local_datasource.dart';
+import 'package:flutter_todo_list_dom/features/tasks/domain/entities/todo.dart';
+import 'package:flutter_todo_list_dom/features/tasks/domain/repositories/task_repository.dart';
+
+class TaskRepositoryImpl implements TaskRepository {
+  final TaskLocalDatasource taskLocalDatasource;
+
+  TaskRepositoryImpl({required this.taskLocalDatasource});
+
+  @override
+  Future<Either<Failure, Todo>> createTask({
+    required String title,
+    String? content,
+    required int priority,
+  }) async {
+    try {
+      final todo =
+          Todo.initialize(title: title, priority: priority, content: content);
+      await taskLocalDatasource.createTask(todo: todo);
+      return Right(todo);
+    } catch (e, s) {
+      print(e);
+      print(s);
+      return Left(Failure(e.toString()));
+    }
+  }
+
+  @override
+  Future<Either<Failure, bool>> deleteTask({required String id}) async {
+    try {
+      await taskLocalDatasource.deleteTask(id: id);
+      return const Right(true);
+    } catch (e, s) {
+      print(e);
+      print(s);
+      return Left(Failure(e.toString()));
+    }
+  }
+
+  @override
+  Future<Either<Failure, Todo>> updateTask({
+    String? title,
+    String? content,
+    int? priority,
+    required String id,
+  }) async {
+    try {
+      final todo = await taskLocalDatasource.updateTask(
+        id: id,
+        title: title,
+        content: content,
+        priority: priority,
+      );
+      return Right(todo);
+    } catch (e, s) {
+      print(e);
+      print(s);
+      return Left(Failure(e.toString()));
+    }
+  }
+}
